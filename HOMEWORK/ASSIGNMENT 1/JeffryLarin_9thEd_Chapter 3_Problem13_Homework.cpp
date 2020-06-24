c37d9a6d942236e36523a7de1fff3eb49301ac75
JEFFRYLARIN2533114 <JLARIN1@STUDENT.RCCD.EDU>
2020-06-23 18:28:06 -0700

    Signed-off-by: JEFFRYLARIN2533114 <JLARIN1@STUDENT.RCCD.EDU>

---
+++ HOMEWORK/ASSIGNMENT 1/JeffryLarin_9thEd_Chapter 3_Problem13_Homework.cpp
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: Jeffry
+ *
+ * Created on June 23, 2020, 5:39 PM
+ */
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() 
+{
+    
+int totalAge;
+cout <<setw(12)<<setprecision(4)<< fixed << totalAge;
+    return 0;
+    
+}
+
